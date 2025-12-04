e77ceeb28cd098ab8db08313c8ff323cb2419c25
LU Yanru <39545418+Lu-Yanru@users.noreply.github.com>
2025-12-04 20:02:44 +0100

    update tester so all code compiles together

--- test.c
+++ test.c
@@ -6,7 +6,7 @@
 /*   By: yanlu <yanlu@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/11/24 12:49:09 by yanlu             #+#    #+#             */
-/*   Updated: 2025/12/04 19:49:47 by yanlu            ###   ########.fr       */
+/*   Updated: 2025/12/04 20:01:35 by yanlu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,18 +73,18 @@ int	main(void)
 	int	src1[10] = {-1, -2, 3, 4, 5, 6, 7, 8, 9, 10};
 	int	dest1[20];
 	int	dest2[20];
-	int	n = 4;
-	memcpy(dest1, src1, sizeof(int) * n);
-	ft_memcpy(dest2, src1, sizeof(int) * n);
+	int	n1 = 4;
+	memcpy(dest1, src1, sizeof(int) * n1);
+	ft_memcpy(dest2, src1, sizeof(int) * n1);
 	int	i = 0;
-	while (i < n)
+	while (i < n1)
 	{
 		printf("%i\n", dest1[i]);
 		i++;
 	}
 	printf("----------\n");
 	i = 0;
-	while (i < n)
+	while (i < n1)
 	{
 		printf("%i\n", dest2[i]);
 		i++;
@@ -92,10 +92,10 @@ int	main(void)
 	printf("===============\n");
 
 	printf("===== memmove =====\n");
-	n = 4;
+	int	n2 = 4;
 	int	src2[10] = {-1, -2, 3, 4, 5, 6, 7, 8, 9, 10};
-	memmove(src1 + 2, src1, sizeof(int) * n);
-	ft_memmove(src2 + 2, src2, sizeof(int) * n);
+	memmove(src1 + 2, src1, sizeof(int) * n2);
+	ft_memmove(src2 + 2, src2, sizeof(int) * n2);
 	i = 0;
 	while (i < 10)
 	{
@@ -109,15 +109,15 @@ int	main(void)
 		printf("%i\n", src2[i]);
 		i++;
 	}
-	printf("===============\n")
+	printf("===============\n");
 
 	printf("===== strlcpy =====\n");
 	char	src3[10] = "a,;bc1234";
 	char	dst3[10];
 	char	dst4[10];
-	int	n = 4;
-	int	res1 = strlcpy(dst3, src3, n);
-	int	res2 = ft_strlcpy(dst4, src3, n);
+	int	n3 = 4;
+	int	res1 = strlcpy(dst3, src3, n3);
+	int	res2 = ft_strlcpy(dst4, src3, n3);
 	printf("%s, %i\n", dst3, res1);
 	printf("%s, %i\n", dst4, res2);
 	printf("================\n");
@@ -129,9 +129,9 @@ int	main(void)
 	//char	dst6[20] = ".\\[]";
 	char	*dst6 = malloc(15 * sizeof(char));
 	memset(dst6, 'r', 15);
-	int	n = 15;
-	int	res3 = strlcat(dst5, src4, n);
-	int	res4 = ft_strlcat(dst6, src4, n);
+	int	n4 = 15;
+	int	res3 = strlcat(dst5, src4, n4);
+	int	res4 = ft_strlcat(dst6, src4, n4);
 	printf("%s, %i\n", dst5, res3);
 	printf("%s, %i\n", dst6, res4);
 	printf("===============\n");
@@ -140,18 +140,18 @@ int	main(void)
 
 	printf("===== strchr =====\n");
 	char	s5[] = "./;a vb123./;";
-	int	c = ' ';
-	char	*res5 = strchr(s5, c);
-	char	*res6 = ft_strchr(s5, c);
+	int	c1 = ' ';
+	char	*res5 = strchr(s5, c1);
+	char	*res6 = ft_strchr(s5, c1);
 	printf("%s\n", res5);
 	printf("%s\n", res6);
 	printf("===============\n");
 
 	printf("===== strrchr =====\n");
 	char	s6[] = "abab";
-	int	c = 'a';
-	char	*res7 = strrchr(s6, c);
-	char	*res8 = ft_strrchr(s6, c);
+	int	c2 = 'a';
+	char	*res7 = strrchr(s6, c2);
+	char	*res8 = ft_strrchr(s6, c2);
 	printf("%s\n", res7);
 	printf("%s\n", res8);
 	printf("===============\n");
@@ -159,45 +159,45 @@ int	main(void)
 	printf("===== strncmp =====\n");
 	char	sa[] = "test\200";
 	char	sb[] = "test\0";
-	int	n = 6;
-	int	resa = strncmp(sa, sb, n);
-	int	resb = ft_strncmp(sa, sb, n);
+	int	n5 = 6;
+	int	resa = strncmp(sa, sb, n5);
+	int	resb = ft_strncmp(sa, sb, n5);
 	printf("%i\n", resa);
 	printf("%i\n", resb);
 	printf("===============\n");
 
 	printf("===== memchr =====\n");
 	char	s7[5] = "ab12";
-	int	c = 'b';
-	int	n = 10;
-	char	*res9 = memchr(s7, c, n);
-	char	*res10 = ft_memchr(s7, c, n);
+	int	c3 = 'b';
+	int	n9 = 10;
+	char	*res9 = memchr(s7, c3, n9);
+	char	*res101 = ft_memchr(s7, c3, n9);
 	printf("%s\n", res9);
-	printf("%s\n", res10);
+	printf("%s\n", res101);
 	printf("===============\n");
 
 	printf("===== memcmp =====\n");
 	int	s8[5] = {-1, 0, 2, 4, 10};
 	int	s9[5] = {-1, 0, 2, 4, -10};
-	int	n = 3;
-	printf("%i\n", memcmp(s8, s9, sizeof(int) * n));
-	printf("%i\n", ft_memcmp(s8, s9, sizeof(int) * n));
+	int	n6 = 3;
+	printf("%i\n", memcmp(s8, s9, sizeof(int) * n6));
+	printf("%i\n", ft_memcmp(s8, s9, sizeof(int) * n6));
 	printf("================\n");
 
 	printf("===== strnstr =====\n");
 	char	big[10] = "abcd1234";
 	char	little[5] = "d1";
-	int	len = 4;
-	char	*res9 = strnstr(big, little, len);
-	char	*res10 = ft_strnstr(big, little, len);
-	printf("%s\n", res9);
+	int	len1 = 4;
+	char	*res91 = strnstr(big, little, len1);
+	char	*res10 = ft_strnstr(big, little, len1);
+	printf("%s\n", res91);
 	printf("%s\n", res10);
 	printf("===============\n");
 
 	printf("===== calloc =====\n");
 	int	nmemb = 4;
 	int	*arr = calloc(nmemb, sizeof(int));
-	int	i = 0;
+	i = 0;
 	//printf("%lu\n", SIZE_MAX);
 	//printf("%zu\n", (size_t)-1);
 	while (i < nmemb)
@@ -217,8 +217,8 @@ int	main(void)
 	printf("===== ft_substr =====\n");
 	char	s11[10] = "a,;.123a]";
 	unsigned int	start = 8;
-	int	len = 2;
-	char	*res12 = ft_substr(s11, start, len);
+	int	len2 = 2;
+	char	*res12 = ft_substr(s11, start, len2);
 	printf("%s\n", res12);
 	printf("============\n");
 
@@ -230,30 +230,30 @@ int	main(void)
 	printf("===============\n");
 
 	printf("===== ft_strtrim =====\n");
-	char	s13[] = "b";
+	char	s131[] = "b";
 	char	set[] = "a";
 	//char	*s13 = NULL;
 	//char	*set = NULL;
-	char	*res14 = ft_strtrim(s13, set);
+	char	*res14 = ft_strtrim(s131, set);
 	printf("%s\n", res14);
 	free(res14);
 	printf("===============\n");
 
 	printf("===== ft_split =====\n");
 	char	s14[] = ",,,abcd, 12345,,,[];,";
-	char	c = ',';
-	char	**arr = ft_split(s14, c);
-	int	i = 0;
-	while (arr[i])
+	char	c7 = ',';
+	char	**arr1 = ft_split(s14, c7);
+	i = 0;
+	while (arr1[i])
 	{
-		printf("%s\n", arr[i]);
+		printf("%s\n", arr1[i]);
 		i++;
 	}
 	printf("===============\n");
 
 	printf("====== ft_itoa =====\n");
-	int	n = -2147483648;
-	char	*res15 = ft_itoa(n);
+	int	n8 = -2147483648;
+	char	*res15 = ft_itoa(n8);
 	printf("%s\n", res15);
 	printf("===============\n");
 
@@ -269,7 +269,7 @@ int	main(void)
 	printf("===============\n");
 
 	printf("===== ft_putnbr_fd =====\n");
-	ft_putnbr_fd(n, 1);	
+	ft_putnbr_fd(n8, 1);	
 	printf("\n===============\n");
 
 
